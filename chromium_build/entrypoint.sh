e1e8b04f2a083275cdbe960f7a8b0673a2dfbe2e
Pierce Freeman <piercefreeman@gmail.com>
2023-08-26 10:16:00 -0700

    Improve patching commands

--- chromium_build/entrypoint.sh
+++ chromium_build/entrypoint.sh
@@ -11,8 +11,29 @@ if [ "$1" = "build" ]; then
     gclient runhooks
 
     # Patching Chromium
-    sed -i 's/^\(\s\+\)PLOG(WARNING) << "poll";$/\1PLOG(WARNING) << "poll"; failed_polls = 0;/' /srv/source/chromium/src/content/browser/sandbox_ipc_linux.cc
-    sed -i 's/^\(\s*\)CHECK(render_process_host->InSameStoragePartition(.*$//*CHECK(render_process_host->InSameStoragePartition(browser_context->GetStoragePartition(site_instance, false)));*/' /srv/source/chromium/src/content/browser/renderer_host/render_process_host_impl.cc
+    awk '{
+    if ($0 ~ /^\s+PLOG\(WARNING\) << "poll";$/) {
+        match($0, /^(\\s+)/, arr);
+        leading_space = arr[1];
+        print leading_space "PLOG(WARNING) << \"poll\"; failed_polls = 0;";
+    } else {
+        print $0;
+    }
+    }' /srv/source/chromium/src/content/browser/sandbox_ipc_linux.cc > tmp_file && mv tmp_file /srv/source/chromium/src/content/browser/sandbox_ipc_linux.cc
+
+    awk 'BEGIN {flag=0} {
+    if (flag == 0 && $0 ~ /CHECK\(render_process_host->InSameStoragePartition\(/) {
+        print "/*";
+        print $0;
+        flag = 1;
+    } else if (flag == 1 && $0 ~ /false\)\)\);/) {
+        print $0;
+        print " */";
+        flag = 0;
+    } else {
+        print $0;
+    }
+    }' /srv/source/chromium/src/content/browser/renderer_host/render_process_host_impl.cc > tmp_file && mv tmp_file /srv/source/chromium/src/content/browser/renderer_host/render_process_host_impl.cc
 
     # Create the build configuration directory
     mkdir -p /srv/source/chromium/src/out/Default
@@ -21,7 +42,7 @@ if [ "$1" = "build" ]; then
     mv /srv/source/chromium/args.gn /srv/source/chromium/src/out/Default/args.gn
 
     # Generate the build configuration
-    RUN gn gen out/Default
+    gn gen out/Default
 
     # Mounting the filesystem like this will require privileged execution of the docker container
     mount --types tmpfs --options size=100G,nr_inodes=256k,mode=1777 tmpfs /srv/source/chromium/src/out/Default
